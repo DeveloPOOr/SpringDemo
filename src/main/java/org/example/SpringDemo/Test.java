c6f4edc9ff5568535e27f4d38f4bb2535028debc
danilcikilev <bzdanya@gmail.com>
2021-03-03 18:11:27 +0500

    Lesson 13.1

--- src/main/java/org/example/SpringDemo/Test.java
+++ src/main/java/org/example/SpringDemo/Test.java
@@ -1,12 +1,14 @@
 package org.example.SpringDemo;
 
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml"
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+                SpringConfig.class
         );
 
         MusicPlayer player = context.getBean("musicPlayer", MusicPlayer.class);
