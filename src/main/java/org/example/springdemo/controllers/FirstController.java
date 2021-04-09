33ca4c6c041e6dcf4f074b9b1e64f821236a135f
danilcikilev <bzdanya@gmail.com>
2021-04-09 15:38:56 +0300

    Lesson 20 task in class

--- src/main/java/org/example/springdemo/controllers/FirstController.java
+++ src/main/java/org/example/springdemo/controllers/FirstController.java
@@ -3,6 +3,7 @@ package org.example.springdemo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
@@ -25,5 +26,37 @@ public class FirstController {
         String surname = request.getParameter("surname");
         System.out.println("Goodbye " + name + " " + surname);
         return "/first/goodbye";
+    }
+
+    @GetMapping("first/calculator")
+    public String calculate(@RequestParam(name = "a") int a,
+                            @RequestParam(name = "b") int b,
+                            @RequestParam(name = "operation") String operation,
+                            Model model) {
+        int ans = 0;
+        String oper = null;
+        if(operation.equals("multiplication")) {
+            oper = "*";
+            ans = a*b;
+        }
+
+        if(operation.equals("addition")) {
+            oper = "+";
+            ans = a+b;
+        }
+
+        if(operation.equals("subtraction")) {
+            oper = "-";
+            ans = a-b;
+        }
+
+        if(operation.equals("division")) {
+            oper = "/";
+            ans = a/b;
+        }
+
+        model.addAttribute("message", a + " " + oper + " " + b +  " = " + ans);
+
+        return "first/calculator";
     }
 }
