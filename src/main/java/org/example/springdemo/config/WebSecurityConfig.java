184b07fc11b21a6bd21fc2d4f312705e9b702ed9
danilcikilev <bzdanya@gmail.com>
2021-08-30 00:05:23 +0300

    added two roles (user, admin) denied access for simple user to edit info

--- src/main/java/org/example/springdemo/config/WebSecurityConfig.java
+++ src/main/java/org/example/springdemo/config/WebSecurityConfig.java
@@ -1,31 +1,64 @@
 package org.example.springdemo.config;
 
+import org.example.springdemo.model.Roles;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.context.annotation.*;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.*;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Configuration
 @EnableWebSecurity
-public class WebSecurityConfig  extends WebSecurityConfigurerAdapter{
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Bean
     public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build());
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager(
+                User.builder()
+                        .username("user")
+                        .password(passwordEncoder().encode("password"))
+                        .roles("USER")
+                        .build(),
+                User.builder()
+                        .username("admin")
+                        .password(passwordEncoder().encode("admin"))
+                        .roles("ADMIN")
+                        .build()
+        );
+
+
         return manager;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers(HttpMethod.GET, "/people/**").hasAnyRole(Roles.ADMIN.name(), Roles.USER.name())
+                .antMatchers(HttpMethod.POST, "/people/**").hasRole(Roles.ADMIN.name())
+                .antMatchers(HttpMethod.PATCH, "/people/**").hasRole(Roles.ADMIN.name())
+                .antMatchers(HttpMethod.DELETE, "/people/**").hasRole(Roles.ADMIN.name())
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin(Customizer.withDefaults());
+
+    }
+
+    @Bean
+    protected BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
     }
 }
---
+++ src/main/java/org/example/springdemo/model/Roles.java
@@ -0,0 +1,6 @@
+package org.example.springdemo.model;
+
+public enum Roles {
+    ADMIN,
+    USER
+}
